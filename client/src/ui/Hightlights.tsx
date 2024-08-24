d9d10c14052b11828981072fbf70a2d30327020d
selimnill <mrrafi812345@gmail.com>
2024-08-25 02:32:59 +0600

    s

--- client/src/ui/Hightlights.tsx
+++ client/src/ui/Hightlights.tsx
@@ -4,7 +4,7 @@ import { getData } from "../lib";
 import { HighlightsType } from "../../type";
 import { Link } from "react-router-dom";
 import { config } from "../../config";
-import Loading from "./Loading";
+// import Loading from "./Loading";
 import ProductModal from "./ProductModal";
 
 const Hightlights = () => {
@@ -31,43 +31,43 @@ const Hightlights = () => {
   return (
     <div>
       <ProductModal />
-
-      <Container className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {!highlightsData ? (
-          <div>Loading...</div>
-        ) : (
-          <>
-            {highlightsData?.map((item: HighlightsType) => (
-              <div
-                key={item?._id}
-                className="relative h-60 rounded-lg shadow-md cursor-pointer overflow-hidden group"
-              >
+      
+        <Container className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {!highlightsData ? (
+            <div>Loading...</div>
+          ) : (
+            <>
+              {highlightsData?.map((item: HighlightsType) => (
                 <div
-                  className="absolute inset-0 bg-cover bg-center rounded-lg transition-transform duration-300 group-hover:scale-110 animate__animated animate__fadeInLeft"
-                  style={{
-                    backgroundImage: `url(${item?.image})`,
-                    color: item?.color,
-                  }}
-                ></div>
-                <div
-                  className="relative z-10 p-6 flex flex-col justify-between h-full"
-                  style={{ color: item?.color }}
+                  key={item?._id}
+                  className="relative h-60 rounded-lg shadow-md cursor-pointer overflow-hidden group"
                 >
-                  <div>
-                    <h3 className="text-2xl font-bold max-w-44">
-                      {item?.name}
-                    </h3>
-                    <p className="text-base font-bold mt-4">{item?.title}</p>
+                  <div
+                    className="absolute inset-0 bg-cover bg-center rounded-lg transition-transform duration-300 group-hover:scale-110 animate__animated animate__fadeInLeft"
+                    style={{
+                      backgroundImage: `url(${item?.image})`,
+                      color: item?.color,
+                    }}
+                  ></div>
+                  <div
+                    className="relative z-10 p-6 flex flex-col justify-between h-full"
+                    style={{ color: item?.color }}
+                  >
+                    <div>
+                      <h3 className="text-2xl font-bold max-w-44">
+                        {item?.name}
+                      </h3>
+                      <p className="text-base font-bold mt-4">{item?.title}</p>
+                    </div>
+                    <Link to={item?._base} className="text-base font-normal">
+                      {item?.buttonTitle}
+                    </Link>
                   </div>
-                  <Link to={item?._base} className="text-base font-normal">
-                    {item?.buttonTitle}
-                  </Link>
                 </div>
-              </div>
-            ))}
-          </>
-        )}
-      </Container>
+              ))}
+            </>
+          )}
+        </Container>
     </div>
   );
 };
